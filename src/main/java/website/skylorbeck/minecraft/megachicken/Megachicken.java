10baa626f2f935fcb66b66582b154bbf84a88815
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2022-06-17 09:41:25 -0700

    Initial commit

---
+++ src/main/java/website/skylorbeck/minecraft/megachicken/Megachicken.java
@@ -0,0 +1,10 @@
+package website.skylorbeck.minecraft.megachicken;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Megachicken implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/website/skylorbeck/minecraft/megachicken/client/MegachickenClient.java
@@ -0,0 +1,13 @@
+package website.skylorbeck.minecraft.megachicken.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MegachickenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
